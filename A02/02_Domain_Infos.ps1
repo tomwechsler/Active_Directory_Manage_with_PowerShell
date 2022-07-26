7a6a9d2203ac3c0f0c8681f5601f14a087004259
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-26 16:41:10 +0200

    new file

--- A02/02_Domain_Infos.ps1
+++ A02/02_Domain_Infos.ps1
@@ -1,2 +1,8 @@
 #Search for some master roles
 Get-ADDomain corp | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator
+
+#Search for some master roles
+Get-ADForest corp | Select-Object DomainNamingMaster, SchemaMaster
+
+#List domaincontrollers and OperationMasterRoles
+Get-ADDomainController -Filter * | Select-Object Name, Domain, Forest, OperationMasterRoles
