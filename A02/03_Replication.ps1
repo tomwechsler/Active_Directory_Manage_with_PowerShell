473d2605de094fe10e83a48b1b9ad9bfcce3a601
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-29 16:35:55 +0200

    lines edited

--- A02/03_Replication.ps1
+++ A02/03_Replication.ps1
@@ -3,3 +3,12 @@ Get-ADReplicationFailure -Target dc01
 
 #For a given domain controller we can find its inbound replication partners
 Get-ADReplicationPartnerMetadata -Target dc01.corp.pri
+
+#We can list down all the inbound replication partners for given domain
+Get-ADReplicationPartnerMetadata -Target "corp.pri" -Scope Domain
+
+#We can review AD replication site objects
+Get-ADReplicationSite -Filter *
+
+#We can review AD replication site links on the AD forest
+Get-ADReplicationSiteLink -Filter *
