9b320e71714238a829cdcdd8e40c4cb9efa60e57
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-29 07:21:16 +0200

    new file lines edited

--- A02/03_Replication.ps1
+++ A02/03_Replication.ps1
@@ -1,2 +1,5 @@
 #Check the replication errors
 Get-ADReplicationFailure -Target dc01
+
+#For a given domain controller we can find its inbound replication partners
+Get-ADReplicationPartnerMetadata -Target dc01.corp.pri
