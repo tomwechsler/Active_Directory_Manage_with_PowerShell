ed0edb8926a0a24af977b4e86391ccc13f80f3f1
Tom Wechsler <tom.wechsler@outlook.com>
2022-07-31 15:27:35 +0200

    added lines

--- A02/02_Domain_Infos.ps1
+++ A02/02_Domain_Infos.ps1
@@ -2,10 +2,16 @@
 Get-ADDomain corp | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator
 
 #The PDCEmulator is important to know for the Event IDs: 4625 and 4750
-#These contain information about password and account blocking
+#These contain information about bad password and account blocking
 
 #Search for some master roles
 Get-ADForest corp | Select-Object DomainNamingMaster, SchemaMaster
 
 #List domaincontrollers and OperationMasterRoles
 Get-ADDomainController -Filter * | Select-Object Name, Domain, Forest, OperationMasterRoles
+
+#Search RSAT tool
+Get-WindowsCapability -Online | Where-Object -Property Name -Like *group*
+
+#Install RSAT Tools
+Add-WindowsCapability -Online -Name Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0
