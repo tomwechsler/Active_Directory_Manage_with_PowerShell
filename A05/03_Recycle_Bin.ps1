841908c69e55277f7d6b870e9c0e605da5e70eb2
Tom Wechsler <tom.wechsler@outlook.com>
2022-08-04 08:52:29 +0200

    new file

---
+++ A05/03_Recycle_Bin.ps1
@@ -0,0 +1,28 @@
+#Gather Information
+Get-ADOptionalFeature -Identity "Recycle Bin Feature"
+
+#Enable
+Enable-ADOptionalFeature `
+–Identity "Recycle Bin Feature" `
+–Scope ForestOrConfigurationSet `
+–Target "corp.int" `
+-Confirm:$False
+
+#Search for a user
+Get-ADUser -Identity "Boris.Jones"
+
+#Delete
+Get-ADUser -Identity "Boris.Jones" | Remove-ADUser -Confirm:$False
+
+#We check
+Get-ADUser -Identity "Boris.Jones"
+
+#We look closer
+Get-ADObject -Filter {Name -like "Boris Jones*"} –IncludeDeletedObjects
+
+#Restore
+Get-ADObject -Filter {Name -like "Boris Jones*"} `
+-IncludeDeletedObjects | Restore-ADObject
+
+#We check
+Get-ADObject -Filter {Name -like "Boris Jones"}
